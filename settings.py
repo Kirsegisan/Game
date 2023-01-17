4ebf9d842d073d123e1c62c26433bc7874d9b6a7
kira <zhulin@1060.ru>
2023-01-17 17:13:20 +0300

    commit

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import math
 
-#game
+# game
 width = 1400
 height = 700
 half_width = width // 2
@@ -9,7 +9,7 @@ FPS = 120
 tile = 100
 fps_pos = (width - 65, 5)
 
-#ray casting
+# ray casting
 fov = math.pi / 3
 half_fov = fov / 2
 num_rays = 700
@@ -19,22 +19,22 @@ dist = num_rays / (2 * math.tan(half_fov))
 proj_coeff = dist * tile
 scale = width // num_rays
 
-#player
+# player
 player_pos = (half_width, half_height)
 player_angle = 0
 
-#color
-white = (255,255,255)
-black = (0,0,0)
-red = (220,0,0)
-green = (70,120,50)
-blue = (0,0,220)
-darkgray = (110,110,110)
-purple = (120,0,120)
-skyblue = (0,50, 155)
-yellow = (220,220,0)
+# color
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (220, 0, 0)
+green = (70, 120, 50)
+blue = (0, 0, 220)
+darkgray = (110, 110, 110)
+purple = (120, 0, 120)
+skyblue = (0, 50, 155)
+yellow = (220, 220, 0)
 
-#mini_map
+# mini_map
 map_scale = 5
 map_tile = tile // map_scale
 map_pos = (0, height // map_scale - height // map_scale)
